818ae504086cc19cd45df8c7885fc6e6772fe4aa
Marco Antônio <marcomartins06@rede.ulbra.br>
2023-04-14 17:51:24 -0300

    Deletando uma tarefa específica - aula 101

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -5,46 +5,56 @@ import 'package:intl/intl.dart';
 import '../models/todo.dart';
 
 class TodoListItem extends StatelessWidget {
-  const TodoListItem({Key? key, required this.todo}) : super(key: key);
+  const TodoListItem({
+    Key? key,
+    required this.todo,
+    required this.onDelete,
+  }) : super(key: key);
 
   final Todo todo;
+  final Function(Todo) onDelete;
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(
-      actionExtentRatio: 0.25,
-      actionPane: const SlidableStrechActionPane(),
-      secondaryActions: [
-        IconSlideAction(
-          color: Colors.red,
-          icon: Icons.delete,
-          onTap: () {},
-        ),
-      ],
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(4),
-          color: Colors.grey[200],
-        ),
-        margin: EdgeInsets.symmetric(vertical: 2),
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
-              style: TextStyle(
-                fontSize: 12,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2),
+      child: Slidable(
+        actionExtentRatio: 0.20,
+        actionPane: const SlidableStrechActionPane(),
+        secondaryActions: [
+          IconSlideAction(
+            color: Colors.red,
+            icon: Icons.delete,
+            caption: 'Deletar',
+            onTap: () {
+              onDelete(todo);
+            },
+          ),
+        ],
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(4),
+            color: Colors.grey[200],
+          ),
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                DateFormat('dd/MM/yyyy - HH:mm').format(todo.dateTime),
+                style: TextStyle(
+                  fontSize: 12,
+                ),
               ),
-            ),
-            Text(
-              todo.title,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
+              Text(
+                todo.title,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
